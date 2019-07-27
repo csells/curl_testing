34b5756788983569beb2383e00e0e8c5af682794
Chris Sells <csells@sellsbrothers.com>
2019-07-27 16:18:26 -0700

    updated for better testing URL replacement

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:curl_testing/json_diff/json_diff.dart';
-import 'package:curl_testing/curl_parser.dart';
 
 // from https://github.com/google/dart-json_diff
 void printDiff(Map<String, dynamic> curlJsonMap, Map<String, dynamic> dartJsonMap) {
@@ -10,6 +9,10 @@ void printDiff(Map<String, dynamic> curlJsonMap, Map<String, dynamic> dartJsonMa
   dartJsonMap['headers'].remove('user-agent');
   if (curlJsonMap['headers']['accept'] == '*/*' && dartJsonMap['headers']['accept'] == null) {
     dartJsonMap['headers']['accept'] = '*/*';
+  }
+  if (curlJsonMap['headers']['accept-encoding'] == 'gzip, deflate, sdch' &&
+      dartJsonMap['headers']['accept-encoding'] == 'gzip') {
+    curlJsonMap['headers'].remove('accept-encoding');
   }
   if (curlJsonMap['headers']['accept-encoding'] == 'deflate, gzip' &&
       dartJsonMap['headers']['accept-encoding'] == 'gzip') {
@@ -61,6 +64,60 @@ final endQuotesRE = RegExp('(\'|")+\$');
 String trimQuotes(String s) => s.replaceAll(begQuotesRE, '').replaceAll(endQuotesRE, '');
 List<String> trimAllQuotes(List<String> ss) => ss.map((s) => trimQuotes(s)).toList();
 
+// from https://github.com/yargs/yargs-parser/blob/master/lib/tokenize-arg-string.js
+// take an un-split argv string and tokenize it.
+List<String> tokenizeArgString(String argString) {
+  argString = argString.trim();
+
+  var i = 0;
+  String prevC;
+  String c;
+  String opening;
+  var args = List<String>();
+
+  for (var ii = 0; ii < argString.length; ii++) {
+    prevC = c;
+    c = argString[ii];
+
+    // split on spaces unless we're in quotes.
+    if (c == ' ' && opening == null) {
+      if (prevC != ' ') i++;
+      continue;
+    }
+
+    // don't split the string if we're in matching
+    // opening or closing single and double quotes.
+    if (c == opening) {
+      opening = null;
+    } else if ((c == "'" || c == '"') && opening == null) {
+      opening = c;
+    }
+
+    if (args.length == i) args.add('');
+    args[i] += c;
+  }
+
+  return args;
+}
+
+// from https://github.com/NickCarneiro/curlconverter/blob/master/util.js
+int getCurlUrlIndex(List<String> args) {
+  assert(args[0] == 'curl');
+  var i = 1;
+
+  // if url argument wasn't where we expected it, try to find it in the other arguments
+  if (args[i].isEmpty || args[i].startsWith('-')) {
+    for (var j = 2; j < args.length; j++) {
+      if (args[j].startsWith('http') || args[j].startsWith('www.')) {
+        i = j;
+        break;
+      }
+    }
+  }
+
+  return i;
+}
+
 // zsh$ for file in dart_output/*.dart; echo $file: `dart bin/main.dart $file | grep DIFF`
 void main(List<String> args) {
   if (args.length != 1) {
@@ -68,22 +125,21 @@ void main(List<String> args) {
     exit(1);
   }
 
+  // read the Dart file w/ the curl command as the first comment at the top
   var s = File(args[0]).readAsStringSync();
-  var re = RegExp(r'https?:\/\/[^\/]*\/');
-
-  // NOTE: for testing purposes, make sure that the first line of the Dart file with the comment containing the curl command
-  // contains a host that ends in / or the temp Dart file below is going to be screwed up (sorry... regexes are hard!);
-  // NOTE2: also make sure that the URL from the curl command is the same in the expected Dart code, too!
-  // NOTE3: the right code will still be generated!
-  assert(s.split('\n')[0].contains(re));
-
-  // replace all of the URLs to point to localhost
-  // NOTE: this assumes test/http-request-dump.js running on localhost:8080
-  s = s.replaceAll(re, 'http://localhost:8080/');
-
-  // execute curl
   var curlCmd = s.split('\n')[0].substring(3);
   var curlArgs = tokenizeArgString(curlCmd);
+
+  // replace all of the curl URLs to point to localhost (including in the url arg)
+  var curlUrlArgIndex = getCurlUrlIndex(curlArgs);
+  assert(curlArgs[curlUrlArgIndex].isNotEmpty);
+  curlArgs[curlUrlArgIndex] = Uri.parse(trimQuotes(curlArgs[curlUrlArgIndex])).toString();
+  var curlUrlBase = RegExp('(https?:\/\/[^\/]*)\/?').firstMatch(curlArgs[curlUrlArgIndex]).group(0);
+  s = s.replaceAll(curlUrlBase, 'http://localhost:8080/');
+  curlArgs[curlUrlArgIndex] =
+      curlArgs[curlUrlArgIndex].replaceFirst(curlUrlBase, 'http://localhost:8080/');
+
+  // execute curl
   var curlJsonMap = runCmd(curlArgs[0], [...curlArgs.skip(1).toList(), '-s']);
 
   // execute dart
