c0dc03ee23426a0d796cde11560c79efadee5faf
Chris Sells <csells@sellsbrothers.com>
2019-07-27 10:29:47 -0700

    automated output of curl and dart output

--- bin/main.dart
+++ bin/main.dart
@@ -1,25 +1,39 @@
-// ignore: avoid_relative_lib_imports
-import '../lib/delete.dart' as delete;
-import '../lib/get_basic_auth_no_user.dart' as get_basic_auth_no_user;
-import '../lib/get_basic_auth.dart' as get_basic_auth;
-import '../lib/get_charles_syntax.dart' as get_charles_syntax;
-import '../lib/get_complex_url_params.dart' as get_complex_url_params;
-import '../lib/get_follow_redirect.dart' as get_follow_redirect;
-import '../lib/get_with_browser_headers.dart' as get_with_browser_headers;
-import '../lib/get_with_single_header.dart' as get_with_single_header;
-import '../lib/get_without_headers.dart' as get_without_headers;
-import '../lib/post_escaped_double_quotes_in_single_quotes.dart'
-    as post_escaped_double_quotes_in_single_quotes;
+import 'dart:io';
+import 'package:csv/csv.dart';
 
-main() {
-  // delete.main();
-  // get_basic_auth_no_user.main();
-  // get_basic_auth.main();
-  // get_charles_syntax.main();
-  // get_complex_url_params.main();
-  // get_follow_redirect.main();
-  // get_with_browser_headers.main();
-  // get_with_single_header.main();
-  // get_without_headers.main();
-  post_escaped_double_quotes_in_single_quotes.main();
+main(List<String> args) {
+  if (args.length != 1) {
+    print('usage: curl_testing <filename>');
+    exit(1);
+  }
+
+  // replace all of the URLs to point to localhost
+  // NOTE: this assumes something running on localhost:8080 for testing
+  var s = File(args[0]).readAsStringSync();
+  var re = RegExp(r'https?:\/\/[^\/]*\/');
+  s = s.replaceAll(re, 'http://localhost:8080/');
+
+  // execute curl
+  var curlCmd = s.split('\n')[0].substring(3);
+  var curlArgs =
+      CsvToListConverter(fieldDelimiter: ' ').convert(curlCmd)[0].map((i) => i.toString()).toList();
+  var curlRes = Process.runSync(curlArgs[0], [...curlArgs.skip(1).toList(), '-s']);
+  print('CURL:***********************************************************************************');
+  print(curlRes.stdout);
+  print(curlRes.stderr);
+
+  // execute dart
+  var packagesFile = File(".packages");
+  var packagesTemplFilename = Directory.systemTemp.path + '/.packages';
+  packagesFile.copySync(packagesTemplFilename);
+
+  var tempFilename = Directory.systemTemp.path + '/curl_testing_temp.dart';
+  var tempFile = File(tempFilename)..writeAsStringSync(s, flush: true);
+  var dartRes = Process.runSync('dart', [tempFile.path]);
+  tempFile.deleteSync();
+  File(packagesTemplFilename).deleteSync();
+
+  print('DART:***********************************************************************************');
+  print(dartRes.stdout);
+  print(dartRes.stderr);
 }
