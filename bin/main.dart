24c588413426d00e80d9358bb15a6fa72fd9a2a4
Chris Sells <csells@sellsbrothers.com>
2019-07-27 11:02:20 -0700

    json format support

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,22 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:csv/csv.dart';
+import 'package:convert/convert.dart';
 
-main(List<String> args) {
+void runCmd(String cmd, List<String> args) {
+  print('${cmd.toUpperCase()}:*******************************************************************');
+  var res = Process.runSync(cmd, args);
+  if (res.stderr.toString().isNotEmpty) throw Exception(res.stderr.toString());
+
+  if (res.stdout[0] == '{') {
+    var encoder = JsonEncoder.withIndent('  ');
+    print(encoder.convert(jsonDecode(res.stdout)));
+  } else {
+    print(res.stdout);
+  }
+}
+
+void main(List<String> args) {
   if (args.length != 1) {
     print('usage: curl_testing <filename>');
     exit(1);
@@ -15,25 +30,17 @@ main(List<String> args) {
 
   // execute curl
   var curlCmd = s.split('\n')[0].substring(3);
-  var curlArgs =
-      CsvToListConverter(fieldDelimiter: ' ').convert(curlCmd)[0].map((i) => i.toString()).toList();
-  var curlRes = Process.runSync(curlArgs[0], [...curlArgs.skip(1).toList(), '-s']);
-  print('CURL:***********************************************************************************');
-  print(curlRes.stdout);
-  print(curlRes.stderr);
+  var csvConverter = CsvToListConverter(fieldDelimiter: ' ');
+  var curlArgs = csvConverter.convert(curlCmd)[0].map((i) => i.toString()).toList();
+  runCmd(curlArgs[0], [...curlArgs.skip(1).toList(), '-s']);
 
   // execute dart
   var packagesFile = File(".packages");
   var packagesTemplFilename = Directory.systemTemp.path + '/.packages';
   packagesFile.copySync(packagesTemplFilename);
-
   var tempFilename = Directory.systemTemp.path + '/curl_testing_temp.dart';
   var tempFile = File(tempFilename)..writeAsStringSync(s, flush: true);
-  var dartRes = Process.runSync('dart', [tempFile.path]);
+  runCmd('dart', [tempFile.path]);
   tempFile.deleteSync();
   File(packagesTemplFilename).deleteSync();
-
-  print('DART:***********************************************************************************');
-  print(dartRes.stdout);
-  print(dartRes.stderr);
 }
